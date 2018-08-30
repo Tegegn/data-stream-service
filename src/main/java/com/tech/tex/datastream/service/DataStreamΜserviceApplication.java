d1d2504cfc9162bcbcdaf5447511a03f338fa731
Tegegn <tegegngelan@gmail.com>
2018-08-30 19:24:45 -0400

    Add files via upload

---
+++ src/main/java/com/tech/tex/datastream/service/DataStreamΜserviceApplication.java
@@ -0,0 +1,12 @@
+package com.tech.tex.datastream.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataStreamΜserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataStreamΜserviceApplication.class, args);
+	}
+}
